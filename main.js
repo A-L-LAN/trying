a599f323f02719bc39d571e7630ad28545560b89
A-L-LAN <144988674+A-L-LAN@users.noreply.github.com>
2023-10-20 22:09:04 +0300

    Update main.js

--- main.js
+++ main.js
@@ -2,10 +2,12 @@
 var firebaseConfig = {
   apiKey: "AIzaSyC21NRWVplPWxq2v2tRVnbNmmzF8ImNDrU",
   authDomain: "upload-334a2.firebaseapp.com",
+  databaseURL: "https://upload-334a2-default-rtdb.firebaseio.com",
   projectId: "upload-334a2",
   storageBucket: "upload-334a2.appspot.com",
   messagingSenderId: "949959553767",
   appId: "1:949959553767:web:2c73a68949e9f832a82842",
+  measurementId: "G-NGVM67Q1SW"
   };
 
 firebase.initializeApp(firebaseConfig);
